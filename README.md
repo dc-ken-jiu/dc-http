977dc303a61a5f4d7ab4cfdb71d3d8de051879c7
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2021-02-01 12:56:11 +0800

    网络请求工具初步封装

---
+++ README.md
@@ -0,0 +1,25 @@
+## 网路请求工具
+
+主要基于axios(0.21.1)二次封装,拓展部分其功能
+
+### API
+
+#### axios
+
+原有的axios
+
+#### http
+
+封装后的用来拓展功能
+
+##### baseGet
+Get请求封装
+
+##### basePost
+Post请求封装
+
+##### basePut
+Put请求封装
+
+##### baseDelete
+Delete请求的封装
--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * @Description:
  * @Author: dingxuejin
  * @Date: 2021-02-01 10:14:54
- * @LastEditTime: 2021-02-01 10:14:57
+ * @LastEditTime: 2021-02-01 12:50:11
  * @LastEditors: dingxuejin
  */
+import axios from "axios";
+import http from './lib/http';
+
+export { http, axios };
